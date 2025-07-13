3487ae2d18dcdb5e43dbc0fd28f5bddb100e276d
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2025-07-12 21:18:23 -0700

    updated main js and css with navbar

--- personal-webapp/src/App.js
+++ personal-webapp/src/App.js
@@ -5,11 +5,15 @@ import RightImage from "./components/rightImg";
 import Social from "./components/social";
 import ScrollIndicator from "./components/scrollIndicator";
 import Copyright from "./components/copyright";
+import Skills from "./components/skills"
+import Projects from "./components/projects";
+import Contact from "./components/contact";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      {/* Section 1 */}
       <div className="main-content">
         <div className="left-container">
           <WelcomeText />
@@ -19,6 +23,19 @@ function App() {
           <RightImage />
         </div>
       </div>
+      {/* Section 2 */}
+      <div className="section2">
+        <Skills />
+      </div>
+      {/* Section 3 */}
+      <div className="section3">
+        <Projects />
+      </div>
+      {/* Section 4 */}
+      <div className="section4">
+        <Contact />
+      </div>
+      {/* Footer */}
       <ScrollIndicator />
       <Copyright />
     </div>
--- personal-webapp/src/components/navbar.jsx
+++ personal-webapp/src/components/navbar.jsx
@@ -2,15 +2,38 @@ import "./navbar.css";
 import AnimatedLogo from "./animatedLogo";
 
 function Navbar() {
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="navbar">
-      <AnimatedLogo />
+      <div className="animated-logo" onClick={() => scrollToSection("home")}>
+        <AnimatedLogo />
+      </div>
       <div className="nav-links">
-        <div className="navbar-about">About</div>
-        <div className="navbar-projects">Projects</div>
-        <div className="navbar-skills">Skills</div>
+        <div
+          className="navbar-skills"
+          onClick={() => scrollToSection("skills")}
+        >
+          Skills
+        </div>
+        <div
+          className="navbar-projects"
+          onClick={() => scrollToSection("projects")}
+        >
+          Projects
+        </div>
         <div className="navbar-resume">Resume</div>
-        <div className="navbar-contact">Contact</div>
+        <div
+          className="navbar-contact"
+          onClick={() => scrollToSection("contact")}
+        >
+          Contact
+        </div>
       </div>
     </div>
   );
